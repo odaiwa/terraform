2b22f306a34c3400ede272c63b14bf6285190de5
Odai Wattad <odaiwa12@gmail.com>
2024-09-04 14:45:35 +0300

    add set and list vars in variables.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
 resource "random_pet" "name" {
-  prefix = var.content["keys1"]
+  prefix = var.content["key1"]
+  separator = var.separator
+}
+
+resource "random_pet" "name1" {
+  prefix = var.content["key1"]
+  separator = var.separator
+}
+
+resource "random_pet" "name2" {
+  prefix = var.content["key1"]
+  separator = var.separator
+}
+
+resource "random_pet" "name3" {
+  prefix = var.content["key1"]
+  separator = var.separator
+}
+
+resource "random_pet" "name4" {
+  prefix = var.content["key1"]
   separator = var.separator
 }
--- variables.tf
+++ variables.tf
@@ -14,7 +14,28 @@ variable "prefies" {
 variable "content" {
   type = map(string)
   default = {
-    "key1": "value1"
-    "key2": "value2"
+    "key1" : "value1"
+    "key2" : "value2"
   }
 }
+
+variable "string-list" {
+  type    = list(string)
+  default = ["value", "value1"]
+}
+
+variable "int-list" {
+  type    = list(number)
+  default = [1, 2]
+}
+
+variable "int-set" {
+  type    = set(number)
+  default = [1, 2]
+}
+
+variable "string-set" {
+  type    = set(string)
+  default = ["val1", "val2"]
+}
+
