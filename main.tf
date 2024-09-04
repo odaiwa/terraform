e6996bb9af9499e1c4d59846505908db9c2f7829
Odai Wattad <odaiwa12@gmail.com>
2024-09-04 15:47:48 +0300

    add explicit dependency

--- main.tf
+++ main.tf
@@ -1,30 +1,37 @@
 resource "random_pet" "name" {
-  prefix = var.content["key1"]
+  prefix    = var.content["key1"]
   separator = var.separator
 }
 
 resource "random_pet" "name1" {
-  prefix = var.content["key1"]
+  prefix    = var.content["key1"]
   separator = var.separator
 }
 
 resource "random_pet" "name2" {
-  prefix = var.content["key1"]
+  prefix    = var.content["key1"]
   separator = var.separator
 }
 
 resource "random_pet" "name3" {
-  prefix = var.content["key1"]
+  prefix    = var.content["key1"]
   separator = var.separator
 }
 
 resource "random_pet" "name4" {
-  prefix = var.content["key1"]
+  prefix    = var.content["key1"]
   separator = var.separator
 }
 
-
 resource "random_pet" "test" {
-  prefix = var.name23
+  prefix    = var.name23
   separator = var.seperator1
+}
+
+
+
+resource "local_file" "pet" {
+  filename = "./here.txt"
+  content  = "the name generated from the prev setp ${random_pet.test.id}"
+  depends_on = [ random_pet.test ]
 }
