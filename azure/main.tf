adb2bcf46bc352b64d6afc0dacfe80cd5a077518
Odai Wattad <odaiwa12@gmail.com>
2024-09-06 14:13:43 +0300

    modify terraform blob storage backup

--- azure/main.tf
+++ azure/main.tf
@@ -12,9 +12,11 @@ terraform {
   }
 }
 
+
+
 resource "azuread_user" "example" {
   user_principal_name = "tf@${var.haat_delivery}"
   display_name        = "terraform user"
-  password            = "Th1s1Saver7R1ndomP@assW@rdF0rTh3U$er1"
+  password            = "Th1s1Saver7R1ndomP@assW@rdF0rTh3U$e12r"
   account_enabled     = true
 }
